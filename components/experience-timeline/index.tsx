6cf1f4635f930bf2335b89453e6053dabea0b5c3
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2026-05-07 14:55:23 +0200

    Fix experience line indicator

--- components/experience-timeline/index.tsx
+++ components/experience-timeline/index.tsx
@@ -35,7 +35,7 @@ export function ExperienceTimeline({ className }: React.ComponentProps<"section"
 
           <motion.div className="pointer-events-none absolute left-0 z-20 h-3 w-full" style={{ top: indicatorTop }}>
             <div
-              className="absolute left-4 h-3 w-3 shrink-0 rounded-full border-2 border-background bg-foreground md:left-1/2 md:-translate-x-1/2"
+              className="absolute left-4 h-3 w-3 shrink-0 -translate-x-1/2 rounded-full border-2 border-background bg-foreground md:left-1/2"
               aria-hidden
             />
           </motion.div>
